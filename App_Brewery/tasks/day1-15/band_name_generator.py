6dda9ec5644889fb645ce509fb7bb9dd60fa3b4d
per5cm <per5cm@proton.me>
2025-08-11 21:32:50 +0200

    band name

---
+++ App_Brewery/tasks/day1-15/band_name_generator.py
@@ -0,0 +1,9 @@
+# small python Project, make a band name generator.
+
+print("\nWelcome to the Band Name Generator.")
+
+city_name_question = input("\nWhat's the name of the city yoz grew up in?")
+
+pets_name_question = input("\nWhat's your pet's name?")
+
+print("Your band name could be:" + city_name_question + " " + pets_name_question)
