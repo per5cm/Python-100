5d975e7b14e06c08bbb27e29b72a5cde519f0965
per5cm <per5cm@proton.me>
2025-08-11 21:33:44 +0200

    smol fix

--- App_Brewery/tasks/day1-15/band_name_generator.py
+++ App_Brewery/tasks/day1-15/band_name_generator.py
@@ -2,8 +2,8 @@
 
 print("\nWelcome to the Band Name Generator.")
 
-city_name_question = input("\nWhat's the name of the city yoz grew up in?")
+city_name_question = input("What's the name of the city yoz grew up in?\n")
 
-pets_name_question = input("\nWhat's your pet's name?")
+pets_name_question = input("What's your pet's name?\n")
 
 print("Your band name could be:" + city_name_question + " " + pets_name_question)
