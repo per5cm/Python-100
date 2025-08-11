fb9018368336d8fe3acc56657131df545865313d
per5cm <per5cm@proton.me>
2025-08-11 20:31:04 +0200

    variables

---
+++ App-Brewery/tasks/day1-15/variables.py
@@ -0,0 +1,13 @@
+# practice variables
+
+# name = "Jack"
+# print(name)
+
+# input = "What is your name?"
+# print(name)
+
+print(len(name = input("whats is your name?")))
+
+# username = input("What is your name?")
+# lenght = len(username)
+# print(lenght)
