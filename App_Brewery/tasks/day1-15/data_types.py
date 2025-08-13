b0efbd22b587f4dcab5fa8b34fcdc29684d74590
per5cm <per5cm@proton.me>
2025-08-13 21:53:20 +0200

    data tpyes

---
+++ App_Brewery/tasks/day1-15/data_types.py
@@ -0,0 +1,25 @@
+# Strings are also data types.
+# there is more data types than strings.
+
+# Basic data types are: Strings, Integer, Float and Boolean
+
+# Subscripting - you can also use negative number for example -2
+print("Hello"[0])
+
+
+# String
+print("123" + "345")
+
+
+# Interger - Whole number
+print(123 + 345)
+
+# Large Intergers
+print(123_456_789)
+
+# Float - Floating point number
+print(3.12159)
+
+# Boolean
+print(True)
+print(False)
