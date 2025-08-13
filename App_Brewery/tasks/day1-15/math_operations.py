8059fa44d1243628ce9e19105998889d90528a5a
per5cm <per5cm@proton.me>
2025-08-13 23:13:37 +0200

    math operators

---
+++ App_Brewery/tasks/day1-15/math_operations.py
@@ -0,0 +1,21 @@
+# Mathematical Operations
+
+print("My age: " + str(12))
+print(123 + 456)
+print(7 - 3)
+print(3 * 2)
+print(6 / 3) # devide always gives float number, its default python behavior
+print(6 // 3) # carefull with this division
+print(2 ** 3) # power of
+
+# Pemndas (oder in which the line of code will be readed):
+# ()
+# **
+# * or /
+# + or - 
+
+print(3 * 3 + 3 / 3 - 3) # =7
+
+print(3 * 3 / 3 + 3 - 3) # =3
+
+print(3 * (3 + 3) / 3 - 3) #=3
