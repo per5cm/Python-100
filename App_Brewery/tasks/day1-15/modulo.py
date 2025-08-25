3c8e9859d9f8f47c7fad3bb8490f56ab1e0ae55c
per5cm <per5cm@proton.me>
2025-08-26 00:30:27 +0200

    modulo: add new

---
+++ App_Brewery/tasks/day1-15/modulo.py
@@ -0,0 +1,14 @@
+# Modulo 10 % 5 = 2
+
+# 10 % 3 = 3 with 1 remaining = 1
+
+# Even number 12 % 2 == 0
+
+print(10 % 3)
+
+number_to_check = int(input("What is the number you want to check? "))
+
+if number_to_check % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
