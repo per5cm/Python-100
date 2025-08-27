8a4fa4a6036fe794ef09078ff696166923b33a6a
per5cm <per5cm@proton.me>
2025-08-27 20:53:03 +0200

    bmi

--- App_Brewery/tasks/day1-15/p_bmi_calculation.py
+++ App_Brewery/tasks/day1-15/p_bmi_calculation.py
@@ -1,8 +1,16 @@
 # Body mass index
 
-height = 1.65
+height = 1.85
 weight = 84
 
 bmi = weight / (height ** 2)
 
 print(bmi)
+
+if bmi < 18.5:
+    print("Your are Underweight!")
+elif bmi >= 18.5 and bmi < 25:
+    print("Your weight is normal!")
+else:
+    print("You are Overweight!")
+    
