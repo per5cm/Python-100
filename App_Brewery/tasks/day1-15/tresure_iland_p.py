d3abde8362587a9fe6596d83fe03e7b693be125e
per5cm <per5cm@proton.me>
2025-10-08 23:27:08 +0200

    bump

--- App_Brewery/tasks/day1-15/tresure_iland_p.py
+++ App_Brewery/tasks/day1-15/tresure_iland_p.py
@@ -32,6 +32,7 @@ print("Your mission is to find the treasure.")
 
 print("\nYou're at a cross road. Where do you want to go?")
 
+## my solution. bit messy.
 direction = input("Type 'left' or 'right': \n").lower()
 
 if direction == "right":
@@ -55,9 +56,22 @@ elif direction == "left":
         else:
             print("Game Over.")
 
-choice1 = input("\nYou're at a cross road. Where do you want to go? Type 'left' or 'right': \n").lower()
+## the other way.
+# choice1 = input("\nYou're at a cross road. Where do you want to go? Type 'left' or 'right': \n").lower()
 
-if choice1 == "left":
-    coice2 = input("You\'ve come to a lake. There is an island in the middle of the lake.Type 'swim' or 'wait'?: \n").lower()
-else:
-    print("Fall into a hole. Game Over")
+# if choice1 == "left":
+#     choice2 = input("You\'ve come to a lake. There is an island in the middle of the lake.Type 'swim' or 'wait'?: \n").lower()
+#     if choice2 == "wait":
+#         choice3 = input("You arrive at the island unharmed. There is a house with 3 doors.One 'red', one 'yellow' and one 'blue'. Which colour do you choose?: \n").lower()
+#         if choice3 == "yellow":
+#             print("You found the treasure. You Win!")
+#         elif choice3 == "red":
+#             print("Its a room full of fire. Game Over.")
+#         elif choice3 == "blue":
+#             print("You enter a room of beasts. Game Over.")
+#         else:
+#             print("You chose a door that doesnt exist. Game Over.")
+#     else:
+#         print("You got attacked by an angry trout. Game Over.")
+# else:
+#     print("Fall into a hole. Game Over.")
