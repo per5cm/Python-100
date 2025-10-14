a05b637150952f691d9d6374af2f8442f393d0af
per5cm <per5cm@proton.me>
2025-10-14 18:22:01 +0200

    head or tails

--- App_Brewery/tasks/day1-15/day4_random_modules.py
+++ App_Brewery/tasks/day1-15/day4_random_modules.py
@@ -4,7 +4,19 @@
 import random
 import day4_my_module
 
-random_integer = random.randit(1, 10)
-print(random_integer)
+# random_integer = random.randint(1, 10)
+# print(random_integer)
 
 print(day4_my_module.my_favorite_number)
+
+# random_number_0_to_1 = random.random() * 10 # semi open range
+# print(random_number_0_to_1)
+
+# random_float = random.uniform(1, 10)
+# print(random_float)
+
+random_coin = random.randint(1, 2)
+if random_coin > 1:
+    print("Heads")
+else:
+    print("Tails")
