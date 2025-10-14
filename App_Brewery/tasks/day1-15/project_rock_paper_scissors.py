dd1a71ea5c200e36589e994a6ef5f9120076770f
per5cm <per5cm@proton.me>
2025-10-15 00:10:55 +0200

    fix

--- App_Brewery/tasks/day1-15/project_rock_paper_scissors.py
+++ App_Brewery/tasks/day1-15/project_rock_paper_scissors.py
@@ -1,16 +1,16 @@
 import random
 from project_rock_paper_scissors_ascii import ascii_list
 
-player_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
-print("Player chose: ")
+player_choice = int(input("\nWhat do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: "))
+print("\nPlayer chose: ")
 print(ascii_list[player_choice])
 
 random_index = random.randint(0, 2)
-print(f"Computer chose: {ascii_list[random_index]}")
+print(f"\nComputer chose: {ascii_list[random_index]}")
 
 if player_choice < random_index:
-    print("Computer Wins!")
+    print("Computer Wins!\n")
 elif player_choice > random_index:
-    print("Player Wins!")
+    print("Player Wins!\n")
 else:
-    print("Its a Draw!")
+    print("Its a Draw!\n")
