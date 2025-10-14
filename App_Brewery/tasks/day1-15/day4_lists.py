c0d3d7dd7c740b3cc75b1bf19241a1178f3252c8
per5cm <per5cm@proton.me>
2025-10-14 18:50:14 +0200

    randoms

---
+++ App_Brewery/tasks/day1-15/day4_lists.py
@@ -0,0 +1,9 @@
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey"]
+
+states_of_america[1] = "Pencilvania"
+
+states_of_america.append("Angelaland")
+
+states_of_america.extend(["Angelaland", "Jack Baver Land"])
+
+print(states_of_america)
---
+++ App_Brewery/tasks/day1-15/project_random_name_fom_list.py
@@ -0,0 +1,5 @@
+import random
+
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+print(random.choices(friends, weights=None, cum_weights=None, k=1))
