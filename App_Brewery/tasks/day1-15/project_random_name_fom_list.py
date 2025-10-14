bad3c858bfb35ccf161f9a66e5207cd47cb08bc2
per5cm <per5cm@proton.me>
2025-10-14 18:57:51 +0200

    bump

--- App_Brewery/tasks/day1-15/project_random_name_fom_list.py
+++ App_Brewery/tasks/day1-15/project_random_name_fom_list.py
@@ -3,3 +3,10 @@ import random
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
 
 print(random.choices(friends, weights=None, cum_weights=None, k=1))
+
+print(random.choice(friends))
+
+# 2nd Option
+
+random_index = random.randint(0, 4)
+print(friends[random_index])
