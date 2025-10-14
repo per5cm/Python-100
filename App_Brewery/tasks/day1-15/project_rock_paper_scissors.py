1beaec1c760624ce101baaf6079dc28e4d26734e
per5cm <per5cm@proton.me>
2025-10-15 00:39:15 +0200

    fix

--- App_Brewery/tasks/day1-15/project_rock_paper_scissors.py
+++ App_Brewery/tasks/day1-15/project_rock_paper_scissors.py
@@ -2,15 +2,21 @@ import random
 from project_rock_paper_scissors_ascii import ascii_list
 
 player_choice = int(input("\nWhat do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: "))
-print("\nPlayer chose: ")
-print(ascii_list[player_choice])
+if player_choice >= 0 and player_choice <=2:
+    print(f"Player chose: {ascii_list[player_choice]}")
 
-random_index = random.randint(0, 2)
-print(f"\nComputer chose: {ascii_list[random_index]}")
+computer_choice = random.randint(0, 2)
+print(f"\nComputer chose: {ascii_list[computer_choice]}")
 
-if player_choice < random_index:
+if player_choice >= 3 or player_choice < 0:
+    print("You typed an invalid number. You lose!\n")
+elif player_choice == 0 and computer_choice == 2:
+    print("Player Wins!\n")
+elif computer_choice == 0 and player_choice == 2:
+    print("You lose!\n")
+elif player_choice < computer_choice:
     print("Computer Wins!\n")
-elif player_choice > random_index:
+elif player_choice > computer_choice:
     print("Player Wins!\n")
 else:
     print("Its a Draw!\n")
