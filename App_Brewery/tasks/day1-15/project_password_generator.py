87d99db42f50735bfff254c3f84cfdd1d1c6b5ea
per5cm <per5cm@proton.me>
2025-10-22 20:25:52 +0200

    bump

--- App_Brewery/tasks/day1-15/project_password_generator.py
+++ App_Brewery/tasks/day1-15/project_password_generator.py
@@ -6,6 +6,4 @@ nr_letters = int(input("How many letters would you like in your password?: \n"))
 nr_symbols = int(input("How many symbols would you like?: \n"))
 nr_numbers = int(input("How many numbers would you like?: \n"))
 
-
-
 print(random_password)
