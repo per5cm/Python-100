2df51de8c8ff24636834b03eeb0de955fae5d7ca
per5cm <per5cm@proton.me>
2025-10-22 23:23:15 +0200

    bump

--- App_Brewery/tasks/day1-15/project_password_generator.py
+++ App_Brewery/tasks/day1-15/project_password_generator.py
@@ -1,9 +1,21 @@
+import random
 from project_password_lists import random_password
 
 print("Welcome to the PyPassword Generator!")
 
-nr_letters = int(input("How many letters would you like in your password?: \n"))
-nr_symbols = int(input("How many symbols would you like?: \n"))
-nr_numbers = int(input("How many numbers would you like?: \n"))
+character_list = " "
+
+while(True):
+    nr_letters = int(input("How many letters would you like in your password?: \n"))
+    if(nr_letters == 1):
+        character_list += nr_letters
+    nr_symbols = int(input("How many symbols would you like?: \n"))
+    
+    character_list += nr_symbols
+    nr_numbers = int(input("How many numbers would you like?: \n"))
+    character_list += nr_numbers
+
+password = []
+print(password)
 
 print(random_password)
