12e997bb7b854453b8771ff582d2ee55e2df0e15
per5cm <per5cm@proton.me>
2025-10-23 00:23:49 +0200

    password generator

--- App_Brewery/tasks/day1-15/project_password_generator.py
+++ App_Brewery/tasks/day1-15/project_password_generator.py
@@ -5,14 +5,25 @@ from project_password_lists import symbols
 
 print("Welcome to the PyPassword Generator!")
 
-password_lenght = ""
-
-
 nr_letters = int(input("How many letters would you like in your password?: \n"))
-password_lenght.append(nr_letters)
 nr_symbols = int(input("How many symbols would you like?: \n"))
-password_lenght.append(nr_symbols)
 nr_numbers = int(input("How many numbers would you like?: \n"))
-password_lenght.append(nr_numbers)
 
-print(password_lenght)
+password_list = []
+
+for char in range(1, nr_letters + 1):
+    password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+    password_list.append(random.choice(symbols))
+
+for char in range(1, nr_numbers + 1):
+    password_list.append(random.choice(numbers))
+
+password = ""
+for char in password_list:
+    password += char
+print("char", char)
+
+pwd = ''.join(password_list)
+print(f"Your random password to use is: {pwd}")
