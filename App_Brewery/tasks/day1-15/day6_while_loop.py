7232da3ee29d59e72967bb21a07c9a1f19f51cee
per5cm <per5cm@proton.me>
2025-11-20 22:22:18 +0100

    reeborgs while loop

--- App_Brewery/tasks/day1-15/day6_while_loop.py
+++ App_Brewery/tasks/day1-15/day6_while_loop.py
@@ -12,6 +12,7 @@
 # Infinite loop
 
 # while 5 > 3:
+#     print(5 > 3)
 #     print("Hello World")
 
 # Reeborg's World
@@ -23,3 +24,20 @@ while number_of_hurdles > 0:
     jump()
     number_of_hurdles -= 1
     print(number_of_hurdles)
+
+# Reborg's World 
+while not at_goal():
+    if front_is_clear():
+        move()
+        
+    elif wall_in_front():
+        turn_left()
+        move()
+        turn_right()
+        move()
+        turn_right()
+        move()
+        turn_left()
+        
+
+print("im here!")
