25158d535c9f731983ac2a6a1a85575aafe0401e
per5cm <per5cm@proton.me>
2025-11-20 23:09:44 +0100

    added reborgs last hurdle

--- App_Brewery/tasks/day1-15/day6_while_loop.py
+++ App_Brewery/tasks/day1-15/day6_while_loop.py
@@ -41,3 +41,32 @@ while not at_goal():
         
 
 print("im here!")
+
+# Reborg's World
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+    turn_left()
+    
+    while wall_on_right():
+        move()        
+    turn_right()
+    move()
+    turn_right()
+
+    while front_is_clear():
+        move()
+    turn_left()
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
+        
+
+print("im here!")
