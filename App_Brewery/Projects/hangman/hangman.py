3648a2f10d3965327a279340419aeda999a0c117
per5cm <per5cm@proton.me>
2025-11-27 21:33:56 +0100

    update hangman while loop nested for loop to check letters

--- App_Brewery/Projects/hangman/hangman.py
+++ App_Brewery/Projects/hangman/hangman.py
@@ -6,15 +6,21 @@ print("Welcome to Hangman!\n")
 # TODO 1 Randomly choose a word from the world_list and assign it to a variable called chosen_word. Then print it.
 
 chosen_word = random.choice(hangman_list)
-print(f"Your chosen word is: {chosen_word}")
+word_length = len(chosen_word)
+print(f"Your chosen word is: {chosen_word} it has {word_length} letters in it.\n")
 
 # TODO 2 Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
-guess = input("Guess a letter in a word: ").lower()
+while True:
+    guess = input("Guess a letter in a word: ").lower()
 
 # TODO 3 Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it is "Wrong if its not."
 
+    guess_list = []
 
-for letter in chosen_word:
-    if guess == chosen_word:
-        print("correct")
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            print("correct")
+        elif letter != guess:
+            print("wrong!")
