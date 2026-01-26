d260717f189f893cbefa9a7a38ec1b635ff7cdca
per5cm <per5cm@proton.me>
2026-01-26 11:07:36 +0100

    update blahgblah

--- App_Brewery/Test/blahblah.py
+++ App_Brewery/Test/blahblah.py
@@ -4,4 +4,16 @@
 # else:
 #     print("Passt")
 
+direction = input("Welche wärung möchten wechseln? e = EUR zu dollar, d = Dollar zu euro: ")
+total_sum = float(input("Wie viel geld möchten Sie wechseln?: "))
 
+euro_to_dollar = 1.19
+dollar_to_euro = 0.84
+
+
+if direction == "e":
+    final_sum = round(euro_to_dollar * total_sum)
+elif direction == "d":
+    final_sum = round(dollar_to_euro * total_sum)
+
+print(f"Die wechsell summe ist: €{final_sum:.2f}")
